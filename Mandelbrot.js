1312aabb2a8e35a235d809943169e80ef4c40815
Emran Shafaq <emran.shafaq@gmail.com>
2017-05-10 16:54:15 -0500

    Implemented better coloring with chroma.js

--- Mandelbrot.js
+++ Mandelbrot.js
@@ -1,13 +1,11 @@
 
-
-
  function mandel(px, py){
  	x0 = scaleX(px)
  	y0 = scaleY(py);
  	x = 0.0
  	y = 0.0
  	iteration = 0
- 	max_iteration = 1000
+ 	max_iteration = 250
  	while (x*x + y*y < 2*2 && iteration < max_iteration) {
  		xtemp = x*x - y*y + x0
  		y = 2*x*y + y0
@@ -18,12 +16,11 @@
  	return iteration;
  }
 
-
  function scaleX(input){
  	var output_start = -2.5;
  	var output_end = 1;
  	var input_start = 0;
- 	var input_end = 300;
+ 	var input_end = 400;
 
  	return output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start);
 
@@ -33,16 +30,11 @@
  	var output_start = -1;
  	var output_end = 1;
  	var input_start = 0;
- 	var input_end = 300;
+ 	var input_end = 400;
 
  	return output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start);
 
  }
- var rgb = [];
-
- for(var i = 0; i < 1004; i++)
- 	rgb.push(Math.floor(Math.random() * 255));
-
 
  function draw() {
  	var canvas = document.getElementById('canvas');
@@ -52,18 +44,20 @@
  	if (canvas.getContext) {
 
  		var ctx = canvas.getContext('2d');
- 		var idata = ctx.getImageData(0,0,canvas.width, canvas.height);
+ 		var idata = ctx.getImageData(100,100,canvas.width, canvas.height);
  		var data = idata.data;
+ 		var scale = chroma.scale(['black', 'white']).domain([0,250]);
 
  		for(var x=0; x<canvas.width; x++){
  			for(var y=0; y<canvas.height; y++){
  				var index = (y*canvas.width+x)*4;
  				var result = mandel(x,y);
+ 				var color = scale(result).rgb();
 
- 				data[index+0] = rgb[result]
- 				data[index+1] = rgb[result+1]
- 				data[index+2] = rgb[result+2]
- 				data[index+3] = rgb[result+3]
+ 				data[index+0] = color[0]
+ 				data[index+1] = color[1];
+ 				data[index+2] = color[2];
+ 				data[index+3] = 255;
 
  				
 
